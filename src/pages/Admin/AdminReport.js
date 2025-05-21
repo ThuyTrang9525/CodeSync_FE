3b3715d5ad403abab0037dd2579aec8b4ea4cd23
MinhHoang1225 <minhhoang121225@gmail.com>
2025-05-21 23:13:33 +0700

    doing admin report

---
+++ src/pages/Admin/AdminReport.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Header from '../../components/Admin/AdminHeader';
+import Nav from '../../components/Admin/AdminNav';
+import Report from '../../components/Admin/AdminTableDataReport';
+export default class AdminReport extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <Header />
+        <Nav /> 
+        <div style={{ marginTop: '20px' }}>
+            <Report />  
+        </div>
+      </div>
+    )
+  }
+}
