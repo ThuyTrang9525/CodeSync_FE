7a1415db8e029492371ffe7dbc9e233b00c5ac23
Hasangnguyen <sanghanguyen05@gmail.com>
2025-05-30 16:26:25 +0700

    Fix UI homepage.

--- src/pages/Student/StudentSupportRequests.js
+++ src/pages/Student/StudentSupportRequests.js
@@ -6,6 +6,7 @@ import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Navbar from '../../components/Student/StudentNavBar';
 import SupportRequestFormBody from '../../components/Student/SupportRequestFormBody';
+import '../../assets/css/StudentRequestSupport.css';
 export default function StudentSupportRequest() {
   return (
     <>
